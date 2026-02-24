fffbc6b20d07e94782ef1ec292438f1c98a11320
Swapnil Phalke <71014235+swapnilphalke97@users.noreply.github.com>
2026-02-24 21:38:56 +0530

    Add files via upload

---
+++ out/extension.js
@@ -0,0 +1,75 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = activate;
+exports.deactivate = deactivate;
+const vscode = require("vscode");
+const child_process_1 = require("child_process");
+const path = require("path");
+function getActiveFilePath(uri) {
+    if (uri) {
+        return uri.fsPath;
+    }
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+        return editor.document.fileName;
+    }
+    return undefined;
+}
+function runP4Command(command, filePath) {
+    const cwd = path.dirname(filePath);
+    const p4Cmd = `p4 ${command} "${filePath}"`;
+    vscode.window.withProgress({
+        location: vscode.ProgressLocation.Notification,
+        title: `Running ${p4Cmd}...`,
+        cancellable: false
+    }, async (progress) => {
+        return new Promise((resolve) => {
+            (0, child_process_1.exec)(p4Cmd, { cwd }, (error, stdout, stderr) => {
+                if (error) {
+                    vscode.window.showErrorMessage(`Perforce Error: ${stderr || error.message}`);
+                    resolve();
+                }
+                else {
+                    vscode.window.showInformationMessage(`Perforce: ${stdout.trim() || 'Success'}`);
+                    resolve();
+                }
+            });
+        });
+    });
+}
+function activate(context) {
+    let editDisposable = vscode.commands.registerCommand('p4-vscode-helper.edit', (uri) => {
+        const filePath = getActiveFilePath(uri);
+        if (filePath) {
+            runP4Command('edit', filePath);
+        }
+        else {
+            vscode.window.showErrorMessage('No active file to checkout.');
+        }
+    });
+    let addDisposable = vscode.commands.registerCommand('p4-vscode-helper.add', (uri) => {
+        const filePath = getActiveFilePath(uri);
+        if (filePath) {
+            runP4Command('add', filePath);
+        }
+        else {
+            vscode.window.showErrorMessage('No active file to add.');
+        }
+    });
+    let revertDisposable = vscode.commands.registerCommand('p4-vscode-helper.revert', (uri) => {
+        const filePath = getActiveFilePath(uri);
+        if (filePath) {
+            vscode.window.showWarningMessage(`Are you sure you want to revert ${path.basename(filePath)}?`, 'Yes', 'No').then(selection => {
+                if (selection === 'Yes') {
+                    runP4Command('revert', filePath);
+                }
+            });
+        }
+        else {
+            vscode.window.showErrorMessage('No active file to revert.');
+        }
+    });
+    context.subscriptions.push(editDisposable, addDisposable, revertDisposable);
+}
+function deactivate() { }
+//# sourceMappingURL=extension.js.map
